f66f850a4665ae5e9d07c7103d014d9cd902cff1
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-15 20:57:01 -0600

    Got connection working

--- public/js/peer.js
+++ public/js/peer.js
@@ -3,12 +3,18 @@ var peer = new Peer({key: 'ry9t770xq4hx5hfr'});
   //   console.log('My peer ID is: ' + id);
   // });
 
-  peer.on('open', function(id) {
-    if ($( ".board" ).data( "p-id" ) !== "") {
-      // initiate connection
-    } else {
-      // send p-id to server
-      console.log(id);
-      $.post("/pid", {pid: id});
-    }
-  })
+peer.on('connection', function(conn){
+  console.log("yippee!");
+})
+
+peer.on('open', function(id) {
+  if ($( ".board" ).data( "p-id" ) !== "") {
+    var pId = $(".board" ).data("p-id");
+    var connection = peer.connect(pId);
+
+  } else {
+    // send p-id to server
+    console.log(id);
+    $.post("/pid", {pid: id});
+  }
+})
